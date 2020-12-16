fe52ac42ea04b812acf315051993e5e9bc8200e3
jamie <jzrust@gmail.com>
2020-12-16 18:13:03 -0500

    6:12pm push phase2 complete jz

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require("express");
 
+const cookieParser = require('cookie-parser');
+const csrf = require("csurf");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
+app.use(cookieParser()); // Adding cookieParser() as application-wide middleware
+app.use(express.urlencoded());
+const csrfProtection = csrf({ cookie: true }); // creating csrfProtection middleware to use in specific routes
+
+
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index.pug", {users}); //the second arguement will always be in an obj
+});
+
+app.get("/create", csrfProtection, (req, res) => {
+  const token = req.csrfToken();
+  res.render("create.pug", {users, token}); //the second arguement will always be in an obj
+
+});
+
+app.get("/create-interesting", csrfProtection, (req, res) => {
+  const token = req.csrfToken();
+  res.render("create-interesting.pug", {users, token}); //the second arguement will always be in an obj
+
 });
 
 const users = [
@@ -17,6 +37,8 @@ const users = [
     email: "jill.jack@gmail.com"
   }
 ];
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
